a1c0a9f1b9c633208ddca121de0ec57992f8bf60
Esfandiarshiran <esf.shiran@gmail.com>
2022-09-03 22:54:45 +0430

    Add comments to about_us view

--- Ecommerce_about_us/views.py
+++ Ecommerce_about_us/views.py
@@ -3,10 +3,13 @@ from Ecommerce_setting.models import SiteSetting
 from .models import TeamMembers
 
 
+# Create about_us view
 def about_us(request):
+    # Start -- Fetch data from site management and TeamMember model
     introduce = SiteSetting.objects.first().introduce
     description = SiteSetting.objects.first().description
     team_members = TeamMembers.objects.all()
+    # End -- Fetch data from site management and TeamMember model
 
     # Pass data to about_us templates
     context = {
