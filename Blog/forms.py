013636536648cefedf9484dd27f6c06352e12ba2
Esfandiar_Shiran <59235244+Esfandiarshiran@users.noreply.github.com>
2022-09-17 22:28:22 +0430

    Update forms.py

--- Blog/forms.py
+++ Blog/forms.py
@@ -3,13 +3,14 @@ from django import forms
 from .models import Comment
 
 
+# Initial Forms based on our model
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=50, label='Name')
     email = forms.EmailField(label='Email')
     to = forms.EmailField(label='To')
     comments = forms.CharField(required=False, widget=forms.Textarea, label='Comments')
 
-
+# Comment Form (Just using modelForm to prevent from extera code)
 class CommentForm(forms.ModelForm):
     # an Amazing way to create a form based on our model with all their all features and limitation
     # in this case just we need these three field
@@ -17,7 +18,7 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('name', 'email', 'body')
 
-
+# single form fr searching
 class SearchForm(forms.Form):
     words = forms.CharField(
         widget=forms.TextInput(
